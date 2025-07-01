55015f04c9f372eb79fe4f2edbbee68d4cd89f2d
Artur Peixoto <artur.peixoto@blip.ai>
2025-07-01 15:08:41 -0300

    update serilogOptions

--- Blip.Ai.Bot.Monitoring.Logging/Models/SerilogOptions.cs
+++ Blip.Ai.Bot.Monitoring.Logging/Models/SerilogOptions.cs
@@ -5,7 +5,7 @@ namespace Blip.Ai.Bot.Monitoring.Logging.Models;
 [ExcludeFromCodeCoverage]
 public class SerilogOptions
 {
-    public string Url { get; set; } = string.Empty;
+    public string? Url { get; set; }
     public string? ApiKey { get; set; }
     public string ApplicationName { get; set; } = "DefaultApp";
 }
