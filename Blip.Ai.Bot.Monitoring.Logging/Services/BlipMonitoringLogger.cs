6cc7e2726bcac09ce731cefd63bf8018e4c89e6e
Artur Peixoto <artur.peixoto@blip.ai>
2025-11-21 11:05:05 -0300

    update package and commits sonar

--- Blip.Ai.Bot.Monitoring.Logging/Services/BlipMonitoringLogger.cs
+++ Blip.Ai.Bot.Monitoring.Logging/Services/BlipMonitoringLogger.cs
@@ -6,8 +6,8 @@ using Blip.Ai.Bot.Monitoring.Logging.Models;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
+using Take.Blip.Ai.Bot.Monitoring.Abstractions;
 using Take.Blip.Ai.Bot.Monitoring.Abstractions.Models;
-using IBlipLogger = Take.Blip.Ai.Bot.Monitoring.Abstractions.IBlipLogger;
 using LogEntry = Blip.Ai.Bot.Monitoring.Logging.Models.Logging;
 
 namespace Blip.Ai.Bot.Monitoring.Logging.Services
